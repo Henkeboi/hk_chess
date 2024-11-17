4326ac606e9c6d47ff47f804319649681bc86a40
Henkeboi <hgg1998@gmail.com>
2024-11-17 17:02:41 +0100

    Update main to play

--- main.cpp
+++ main.cpp
@@ -12,7 +12,8 @@
 int main() {
 	Board board {};
 	move::Move best_move{0, 0, 0, 0};
-	int depth = 5;
+	int white_depth = 4;
+	int black_depth = 4;
 	bool maximizing_player = true;
 
 	for (int i = 0; i < 20; ++i) {
@@ -24,7 +25,11 @@ int main() {
 	bool is_game_finished = false;
 	while (!is_game_finished) {
 		visualization::update_visualization(board);
-		alpha_beta::alpha_beta(board, depth, maximizing_player, best_move, depth);
+		if (maximizing_player) {
+			std::println("White eval: {}", alpha_beta::alpha_beta(board, white_depth, maximizing_player, best_move, white_depth));
+		} else {
+			std::println("Black eval: {}", alpha_beta::alpha_beta(board, black_depth, maximizing_player, best_move, black_depth));
+		}
 		board = Board{board, best_move, 0};
 		is_game_finished = board.is_game_finished(!maximizing_player);
 		maximizing_player = !maximizing_player;
