b889834c885fb0b270cf81409d8482338cce965d
Henkeboi <hgg1998@gmail.com>
2024-11-27 17:43:13 +0100

    Fix en passant checking, add asserts and remove overflow.

--- move.cpp
+++ move.cpp
@@ -1,10 +1,21 @@
 #include "move.hpp"
+#include <cassert>
 #include <iostream>
 
 namespace move {
 
-Move::Move(uint8_t from_row, uint8_t from_col, uint8_t to_row, uint8_t to_col, uint8_t promotion_piece)
+Move::Move(uint8_t from_row, uint8_t from_col, uint8_t to_row, uint8_t to_col, uint8_t promotion_piece, bool is_en_passant, bool enables_en_passant)
 : _from(0), _to(0), _promotion_piece(promotion_piece) {
+	assert(from_row < 8);
+	assert(from_col < 8);
+	assert(to_row < 8);
+	assert(to_col < 8);
+
+	if (is_en_passant) 
+		_from |= is_en_passant_bitmask;
+	if (enables_en_passant) 
+		_from |= enables_en_passant_bitmask;
+	
 	_from |= from_row << 3;
 	_from |= from_col;
 	_to |= to_row << 3;
@@ -12,11 +23,7 @@ Move::Move(uint8_t from_row, uint8_t from_col, uint8_t to_row, uint8_t to_col, u
 }
 
 Move::Move(const Move& other)
-: _from(0), _to(0), _promotion_piece(other.get_promotion_piece()) {
-	_from |= other.get_from_row() << 3;
-	_from |= other.get_from_col();
-	_to |= other.get_to_row() << 3;
-	_to |= other.get_to_col(); 
+: _from(other._from), _to(other._to), _promotion_piece(other.get_promotion_piece()) {
 }
 
 void Move::print() const {
@@ -24,25 +31,27 @@ void Move::print() const {
 }
 
 move::Move& Move::operator=(const Move& other) {
-	_from = 0;
-	_to = 0;
-	_from |= other.get_from_row() << 3;
-	_from |= other.get_from_col();
-	_to |= other.get_to_row() << 3;
-	_to |= other.get_to_col(); 
-	_promotion_piece = other.get_promotion_piece();
+	_from = other._from;
+	_to = other._to;
+	_promotion_piece = other._promotion_piece;
 	return *this;
 }
 
 move::Move& Move::operator=(Move& other) {
-	_from = 0;
-	_to = 0;
-	_from |= other.get_from_row() << 3;
-	_from |= other.get_from_col();
-	_to |= other.get_to_row() << 3;
-	_to |= other.get_to_col(); 
+	_from = other._from;
+	_to = other._to;
 	_promotion_piece = other.get_promotion_piece();
 	return *this;
+}
+
+bool Move::operator==(const Move& other) const {
+	if (_to != other._to)
+		return false;
+	if (_from != other._from)
+		return false;
+	if (get_promotion_piece() != other.get_promotion_piece()) 
+		return false;
+	return true;
 }
 
 }
