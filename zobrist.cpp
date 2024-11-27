bc4153db12a6e4d99aecd9ba84c706f2deb24ee9
Henkeboi <hgg1998@gmail.com>
2024-11-27 22:10:35 +0100

    Fix piece movement in zobrist hash calculation

--- zobrist.cpp
+++ zobrist.cpp
@@ -5,8 +5,8 @@
 #include <iostream>
 
 Zobrist::Zobrist(const Board& board, const bool white_to_move)
-: _initial_zobrist_hash(1084901349014850), _uniform_distribution(0, std::numeric_limits<uint64_t>::max()) {
-	std::mt19937_64::result_type const seed = 0xa4535b3446;
+: _initial_zobrist_hash(0), _uniform_distribution(0, std::numeric_limits<uint64_t>::max()) {
+	std::mt19937_64::result_type const seed = 0xa4535b34434526;
 	_rng.seed(seed);
 
 	for (auto piece_index = 0; piece_index < 12; ++piece_index) {
@@ -37,55 +37,51 @@ uint64_t Zobrist::get_initial_zobrist_hash() const {
 }
 
 uint64_t Zobrist::new_zobrist_hash(const Board& board_to_hash, const Board& prev_board, const move::Move& last_move, uint64_t prev_zobrist_hash) const {
-	switch (board_to_hash.get_raw_board()[last_move.get_to_row()][last_move.get_to_col()]) {
-		case (pieces::white | pieces::pawn):
-			prev_zobrist_hash ^= _piece_positions_bits[_white_pawn_index][last_move.get_from_row()][last_move.get_from_col()];
-			prev_zobrist_hash ^= _piece_positions_bits[_white_pawn_index][last_move.get_to_row()][last_move.get_to_col()];
-			break;
-		case (pieces::white | pieces::knight):
-			prev_zobrist_hash ^= _piece_positions_bits[_white_knight_index][last_move.get_from_row()][last_move.get_from_col()];
-			prev_zobrist_hash ^= _piece_positions_bits[_white_knight_index][last_move.get_to_row()][last_move.get_to_col()];
-			break;
-		case (pieces::white | pieces::bishop):
-			prev_zobrist_hash ^= _piece_positions_bits[_white_bishop_index][last_move.get_from_row()][last_move.get_from_col()];
-			prev_zobrist_hash ^= _piece_positions_bits[_white_bishop_index][last_move.get_to_row()][last_move.get_to_col()];
-			break;
-		case (pieces::white | pieces::rook):
-			prev_zobrist_hash ^= _piece_positions_bits[_white_rook_index][last_move.get_from_row()][last_move.get_from_col()];
-			prev_zobrist_hash ^= _piece_positions_bits[_white_rook_index][last_move.get_to_row()][last_move.get_to_col()];
-			break;
-		case (pieces::white | pieces::queen):
-			prev_zobrist_hash ^= _piece_positions_bits[_white_queen_index][last_move.get_from_row()][last_move.get_from_col()];
-			prev_zobrist_hash ^= _piece_positions_bits[_white_queen_index][last_move.get_to_row()][last_move.get_to_col()];
-			break;
-		case (pieces::white | pieces::king):
-			prev_zobrist_hash ^= _piece_positions_bits[_white_king_index][last_move.get_from_row()][last_move.get_from_col()];
-			prev_zobrist_hash ^= _piece_positions_bits[_white_king_index][last_move.get_to_row()][last_move.get_to_col()];
-			break;
-		case (pieces::black | pieces::pawn):
-			prev_zobrist_hash ^= _piece_positions_bits[_black_pawn_index][last_move.get_from_row()][last_move.get_from_col()];
-			prev_zobrist_hash ^= _piece_positions_bits[_black_pawn_index][last_move.get_to_row()][last_move.get_to_col()];
-			break;
-		case (pieces::black | pieces::knight):
-			prev_zobrist_hash ^= _piece_positions_bits[_black_knight_index][last_move.get_from_row()][last_move.get_from_col()];
-			prev_zobrist_hash ^= _piece_positions_bits[_black_knight_index][last_move.get_to_row()][last_move.get_to_col()];
-			break;
-		case (pieces::black | pieces::bishop):
-			prev_zobrist_hash ^= _piece_positions_bits[_black_bishop_index][last_move.get_from_row()][last_move.get_from_col()];
-			prev_zobrist_hash ^= _piece_positions_bits[_black_bishop_index][last_move.get_to_row()][last_move.get_to_col()];
-			break;
-		case (pieces::black | pieces::rook):
-			prev_zobrist_hash ^= _piece_positions_bits[_black_rook_index][last_move.get_from_row()][last_move.get_from_col()];
-			prev_zobrist_hash ^= _piece_positions_bits[_black_rook_index][last_move.get_to_row()][last_move.get_to_col()];
-			break;
-		case (pieces::black | pieces::queen):
-			prev_zobrist_hash ^= _piece_positions_bits[_black_queen_index][last_move.get_from_row()][last_move.get_from_col()];
-			prev_zobrist_hash ^= _piece_positions_bits[_black_queen_index][last_move.get_to_row()][last_move.get_to_col()];
-			break;
-		case (pieces::black | pieces::king):
-			prev_zobrist_hash ^= _piece_positions_bits[_black_king_index][last_move.get_from_row()][last_move.get_from_col()];
-			prev_zobrist_hash ^= _piece_positions_bits[_black_king_index][last_move.get_to_row()][last_move.get_to_col()];
-			break;
+	auto xor_hash_with_piece_value = [this, &prev_zobrist_hash](const Board& board, uint8_t row, uint8_t col) {
+		switch (board.get_raw_board()[row][col]) {
+			case (pieces::white | pieces::pawn):
+				prev_zobrist_hash ^= _piece_positions_bits[_white_pawn_index][row][col];
+				break;
+			case (pieces::white | pieces::knight):
+				prev_zobrist_hash ^= _piece_positions_bits[_white_knight_index][row][col];
+				break;
+			case (pieces::white | pieces::bishop):
+				prev_zobrist_hash ^= _piece_positions_bits[_white_bishop_index][row][col];
+				break;
+			case (pieces::white | pieces::rook):
+				prev_zobrist_hash ^= _piece_positions_bits[_white_rook_index][row][col];
+				break;
+			case (pieces::white | pieces::queen):
+				prev_zobrist_hash ^= _piece_positions_bits[_white_queen_index][row][col];
+				break;
+			case (pieces::white | pieces::king):
+				prev_zobrist_hash ^= _piece_positions_bits[_white_king_index][row][col];
+				break;
+			case (pieces::black | pieces::pawn):
+				prev_zobrist_hash ^= _piece_positions_bits[_black_pawn_index][row][col];
+				break;
+			case (pieces::black | pieces::knight):
+				prev_zobrist_hash ^= _piece_positions_bits[_black_knight_index][row][col];
+				break;
+			case (pieces::black | pieces::bishop):
+				prev_zobrist_hash ^= _piece_positions_bits[_black_bishop_index][row][col];
+				break;
+			case (pieces::black | pieces::rook):
+				prev_zobrist_hash ^= _piece_positions_bits[_black_rook_index][row][col];
+				break;
+			case (pieces::black | pieces::queen):
+				prev_zobrist_hash ^= _piece_positions_bits[_black_queen_index][row][col];
+				break;
+			case (pieces::black | pieces::king):
+				prev_zobrist_hash ^= _piece_positions_bits[_black_king_index][row][col];
+				break;
+		}
+	};
+
+	xor_hash_with_piece_value(prev_board, last_move.get_from_row(), last_move.get_from_col());
+	xor_hash_with_piece_value(board_to_hash, last_move.get_to_row(), last_move.get_to_col());
+	if (prev_board.get_raw_board()[last_move.get_to_row()][last_move.get_to_col()] != pieces::empty) {
+		xor_hash_with_piece_value(prev_board, last_move.get_to_row(), last_move.get_to_col());
 	}
 
 	prev_zobrist_hash ^= _white_to_move_bits;
@@ -103,7 +99,7 @@ uint64_t Zobrist::new_zobrist_hash(const Board& board_to_hash, const Board& prev
 
 	const move::Move prev_move = prev_board.get_last_move();
 	if (prev_move.enables_en_passant() == true)
-		prev_zobrist_hash ^=  _en_passant_files_bits[prev_move.get_from_col()];
+		prev_zobrist_hash ^= _en_passant_files_bits[prev_move.get_from_col()];
 
 	return prev_zobrist_hash;
 }
